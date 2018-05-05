cdeb541c8d2ea45724a1ebddef9c57d1d6e08502
Christopher <christopher.klammt@gmail.com>
2018-05-05 11:07:05 +0200

    Extracted variable frequentRenterPoints - Code Smell: Temporary Field (Object-Orientation Abuser)
    Refactoring technique: Replace Temp with Query

--- src/main/java/de/chris/Customer.java
+++ src/main/java/de/chris/Customer.java
@@ -19,21 +19,19 @@ class Customer {
     }
 
     String statement() {
-        int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();
         StringBuilder result = new StringBuilder("Rental Record for " + this.getName() + "\n");
         result.append("\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n");
 
         while (enum_rentals.hasMoreElements()) {
             Rental each = (Rental) enum_rentals.nextElement();
-            frequentRenterPoints += each.getFrequentRenterPoints();
 
             //show figures for this rental
             result.append("\t").append(each.getMovie().getTitle()).append("\t").append("\t").append(each.getDaysRented()).append("\t").append(String.valueOf(each.getCharge())).append("\n");
         }
         //add footer lines
         result.append("Amount owed is ").append(String.valueOf(getTotalCharge())).append("\n");
-        result.append("You earned ").append(String.valueOf(frequentRenterPoints)).append(" frequent renter points");
+        result.append("You earned ").append(String.valueOf(getTotalFrequentRenterPoints())).append(" frequent renter points");
         return result.toString();
     }
 
@@ -49,5 +47,16 @@ class Customer {
         return result;
     }
 
-}
+    int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration<Rental> enum_rentals = rentals.elements();
 
+        while (enum_rentals.hasMoreElements()) {
+            Rental rental = enum_rentals.nextElement();
+            result += rental.getFrequentRenterPoints();
+        }
+
+        return result;
+    }
+
+}
